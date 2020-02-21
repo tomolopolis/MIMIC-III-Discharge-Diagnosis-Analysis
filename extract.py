0744880882ead47c805bd87f1f2b9540ee56488c
tomolopolis <tsearle88@gmail.com>
2020-02-21 19:55:16 +0000

    More notebooks, more inline with the paper

--- extract.py
+++ extract.py
@@ -13,25 +13,6 @@ logger = logging.getLogger()
 MIMIC_2 = 'mimic2'
 MIMIC_3 = 'mimic3'
 
-
-def mimic2_diagnosis_icd(conn):
-    logger.info("Pulling all ICD9 Codes for MIMIC2 Admissions")
-    columns = ['subject_id', 'hadm_id', 'sequence', 'icd9_code', 'short_title']
-    query = "select subject_id, hadm_id, sequence, replace(code, '.', '') as icd9_code, " \
-            "description as short_title from icd9"
-    return _pull_from_db(conn, [c.split(' ')[-1] for c in columns], query, MIMIC_2,
-                         'admission_codes.csv')
-
-
-def mimic2_notes(conn):
-    logger.info("Pulling all Discharge Summaries for MIMIC2")
-    columns = ['subject_id', 'hadm_id', 'charttime', 'category', 'text']
-    query = f"select {', '.join(columns)} " \
-        " from noteevents where category = 'DISCHARGE_SUMMARY'" \
-        " and regexp_replace(noteevents.text, '\s+', '', 'g') <> ''"
-    return _pull_from_db(conn, columns, query, MIMIC_2, 'notes.csv')
-
-
 def mimic3_diagnosis_icd(conn):
     logger.info("Pulling ICD9 Diagnosis codes for MIMIC3 Admissions")
     columns = ['hadm_id', 'subject_id', 'sequence', 'icd9_code', 'short_title', 'long_title']
@@ -41,7 +22,7 @@ def mimic3_diagnosis_icd(conn):
         from DIAGNOSES_ICD as d_icd, D_ICD_DIAGNOSES as icd
         where d_icd.icd9_code = icd.icd9_code
     """
-    return _pull_from_db(conn, columns, query, MIMIC_3, 'admission_codes.csv')
+    return _pull_from_db(conn, columns, query, 'admission_codes.csv')
 
 
 def mimic3_note_adult_only(conn):
@@ -58,16 +39,24 @@ def mimic3_notes(conn):
     logger.info("Pulling all Discharge Summaries for MIMIC3")
     columns = ['subject_id', 'hadm_id', 'chartdate', 'category', 'description', 'text']
     query = f"select {', '.join(columns)}  from NOTEEVENTS where category='Discharge summary'"
-    return _pull_from_db(conn, columns, query, MIMIC_3, 'notes.csv')
+    return _pull_from_db(conn, columns, query, 'notes.csv')
 
 
-def _pull_from_db(conn, columns, query, data_src, filename):
+def _pull_from_db(conn, columns, query, filename):
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     logger.info(f"Executing Query:\n{query}")
     cursor.execute(query)
     data = pd.DataFrame(np.array(cursor.fetchall()), columns=columns)
-    os.makedirs(f'data/{data_src}/raw', exist_ok=True)
-    data.to_csv(f'data/{data_src}/raw/{filename}', index=False)
+    os.makedirs(f'data/', exist_ok=True)
+    data.to_csv(f'data/{filename}', index=False)
     logger.info(f'Written {filename} to working dir .')
     cursor.close()
     return data
+
+
+def append_addendums(df):
+    join_str = '\n\n'
+    aggs_by_col = {'subject_id': lambda s: list(s)[0], 'chartdate': lambda s: list(s)[0],
+                   'text': lambda s: join_str.join(s)}
+    grouped = df.groupby(by=['hadm_id'], group_keys=False).agg(aggs_by_col).reset_index()
+    return grouped
